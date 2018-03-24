e97354e0db0e57af8f1d183fa55c071a12e89e27
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-03-24 14:39:03 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
 
+  get '/new' do
+    # binding.pry
+    erb :create_puppy
+  end
+
+  post '/display_puppy' do
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    # binding.pry
+    erb :display_puppy
+  end
 end
